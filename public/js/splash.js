5fc67024542ed19e34064bbc52e6f53c399cdd9c
MrHarshPatel <hpatel1223@gmail.com>
2019-03-18 13:05:19 -0400

    Initial Commit, added birds for background

---
+++ public/js/splash.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  VANTA.BIRDS({
+    el: "#main",
+    backgroundColor: 0xffffff,
+    color2: 0x0,
+    colorMode: "lerpGradient",
+    quantity: 4.00
+  })
+})
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const server = app.listen(3000);
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
